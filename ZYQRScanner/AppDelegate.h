76242189b9e763565d4f14b73ef0ddecaf04706d
zhang <hr@51h1h.com>
2016-12-16 10:09:25 +0800

    Initial Commit

---
+++ ZYQRScanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYQRScanner
+//
+//  Created by 换一换 on 16/12/16.
+//  Copyright © 2016年 换一换. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYQRScanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYQRScanner
+//
+//  Created by 换一换 on 16/12/16.
+//  Copyright © 2016年 换一换. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
